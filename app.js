4590b8492436dc3fb427320196a278dcfe1aac29
Cj Jordan <cj.jordan.tech@gmail.com>
2015-01-28 13:59:20 -0500

    Add Controller

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+	.controller('MyCtrl', function(VERSION, $scope) {
+			$scope.femaleName = 'female name';
+			$scope.jobTitle = 'job title';
+			$scope.tediousTask = 'tedious task'; 
+			$scope.dirtyTask = 'dirty task'; 
+			$scope.celebrity = 'celebrity'; 
+			$scope.uselessSkill = 'useless skill';
+			$scope.adjective = 'adjective';
+			$scope.obnoxiousCelebrity = 'obnoxious celebrity';
+			$scope.hugeNumber = 'huge number';
+  });
